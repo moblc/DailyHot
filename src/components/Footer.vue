a97a89a107466526ba779fcc4a520ce2afe94414
moblc <134125241+moblc@users.noreply.github.com>
2025-02-08 23:06:52 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,7 +6,6 @@
         class="author"
         :depth="3"
         v-html="packageJson.author"
-        @click="jumpLink(packageJson.github)"
       />
     </div>
     <n-text
@@ -14,7 +13,6 @@
       :depth="3"
       class="icp"
       v-html="icp"
-      @click="jumpLink('https://beian.miit.gov.cn/')"
     />
   </footer>
 </template>
@@ -23,11 +21,6 @@
 import packageJson from "@/../package.json";
 
 const icp = ref(import.meta.env.VITE_ICP ? import.meta.env.VITE_ICP : null);
-
-// 链接跳转
-const jumpLink = (url) => {
-  window.open(url);
-};
 </script>
 
 <style lang="scss" scoped>
@@ -44,7 +37,6 @@ footer {
     margin-bottom: 4px;
     .description {
       &::after {
-        content: "@ Copyright By";
         margin: 0 6px;
       }
     }
